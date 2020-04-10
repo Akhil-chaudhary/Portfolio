255796602badbcfe2337badc2373c963ee64e0c6
Akhil-chaudhary <akhilcodebugged@gmail.com>
2020-04-10 20:21:41 +0530

    Favicon optimized

--- js/main.js
+++ js/main.js
@@ -166,7 +166,7 @@
                         var $this = $(this);
 
                         $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                            duration: 4000,
+                            duration: 1000,
                             easing: 'swing',
                             step: function (curValue) {
                                 $this.text(Math.ceil(curValue));
@@ -465,7 +465,6 @@
         clAOS();
         clAjaxChimp();
         clBackToTop();
-
     })();
         
         
